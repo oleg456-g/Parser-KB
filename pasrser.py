91b64987abab852c291739917a4e5193a4f39016
oleg456-g <62814783+oleg456-g@users.noreply.github.com>
2022-08-23 18:37:01 +0300

    v0.2
    Улучшил "точность" сбора данных, и небольшая оптимизация.

--- pasrser.py
+++ pasrser.py
@@ -6,22 +6,18 @@ import csv
 def get_info():
     # Настраиваем браузер Chrome
     chrome_options = webdriver.ChromeOptions()
-    prefs = {"profile.default_content_setting_values.notifications": 2}
     chrome_options.add_argument("--disable-notifications")
     # Открываем страницу КБ
     driver = webdriver.Chrome(chrome_options=chrome_options)
     url = "https://krasnoeibeloe.ru/catalog/vino-s-otsenkoy/"
     driver.get(url)
     # Получаем информацию
-    get_vines = driver.find_elements(By.CLASS_NAME, "catalog_product_item_cont")
-    # driver.quit()
+    get_vines_name = driver.find_elements(By.CLASS_NAME, "product_item_name")
+    get_vines_price = driver.find_elements(By.CLASS_NAME, "product_item__price")
+    get_vines_rating = driver.find_elements(By.CLASS_NAME, "bl_rob_p__rating")
 
-    for vine in get_vines:
-        get_text_about_vine = vine.text.split('\n')
-        process_text = [get_text_about_vine[2]] + get_text_about_vine[5:8]
-        yield process_text
-
-    return process_text
+    for i in range(len(get_vines_name)):
+        yield [get_vines_rating[i].text, get_vines_name[i].text.replace('\n', ' '), get_vines_price[i].text]
 
 
 # Функция записи в файл csv
@@ -29,7 +25,7 @@ def main():
     text = get_info()
     with open('vine.csv', 'w', newline="", encoding="utf-8") as file:
         writer = csv.writer(file)
-        writer.writerow(['Баллы', 'Вино', 'Литраж, Страна', 'Цена'])
+        writer.writerow(['Баллы', 'Вино', 'Цена'])
         for info_vine in text:
             print(info_vine)
             writer.writerow(info_vine)
