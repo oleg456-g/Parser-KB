039b3374fc74b0167f214dc7200159e28c5b4ec2
oleg456-g <62814783+oleg456-g@users.noreply.github.com>
2022-09-07 15:04:37 +0300

    v2.0
    Большое обновление теперь можно вставлять любую(почти) ссылку с сайта КБ и он её спарсит, теперь код работает немного шустрее, много новго для себя узнал и проверка введённой ссылки.

--- pasrser.py
+++ pasrser.py
@@ -3,42 +3,50 @@ from selenium.webdriver.common.by import By
 import csv
 
 
-def get_info():
+def get_info(url):
     # Настраиваем браузер Chrome
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--disable-notifications")
 
     get_vines_name = []
     get_vines_price = []
-    get_vines_rating = []
-    page_count = input('Введите количество страниц в разделе --> ')
-    if page_count.isdigit():
-        for a in range(int(page_count)):
-            # Открываем страницу КБ
-            driver = webdriver.Chrome(chrome_options=chrome_options)
-            url = f"https://krasnoeibeloe.ru/catalog/vino-s-otsenkoy/?PAGEN_1={a+1}"
-            # Получаем информацию
-            driver.get(url)
-            get_vines_name += driver.find_elements(By.CLASS_NAME, "product_item_name")
-            get_vines_price += driver.find_elements(By.CLASS_NAME, "product_item__price")
-            get_vines_rating += driver.find_elements(By.CLASS_NAME, "bl_rob_p__rating")
-        # Объединяем информацию из трёх переменных создаём генератор
-        for i in range(len(get_vines_price)):
-            yield [get_vines_rating[i].text, get_vines_name[i].text.replace('\n', ','), get_vines_price[i].text]
-    else:
-        print('Убедитесь, что ввели только цифры!')
+    # Открываем страницу КБ и получаем количество страниц
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+    driver.get(url)
+    pages = driver.find_elements(By.CLASS_NAME, "bl_pagination")
+    # Да, можно было не писать следующие две строчки но так быстрее расботает
+    get_vines_name += driver.find_elements(By.CLASS_NAME, "product_item_name")
+    get_vines_price += driver.find_elements(By.CLASS_NAME, "product_item__price")
+
+    for page in pages[0].text.split('\n')[:-1]:
+        driver = webdriver.Chrome(chrome_options=chrome_options)
+        driver.get(f"{url}?PAGEN_1={int(page)+1}")
+        # Получаем информацию
+        get_vines_name += driver.find_elements(By.CLASS_NAME, "product_item_name")
+        get_vines_price += driver.find_elements(By.CLASS_NAME, "product_item__price")
+
+    # Объединяем информацию из трёх переменных создаём генератор
+    for i in range(len(get_vines_price)):
+        yield [get_vines_name[i].text.replace('\n', ','), get_vines_price[i].text]
 
 
 # Функция записи в файл csv
-def main():
-    text = get_info()
+def main(url):
+    text = get_info(url)
+    # Открываем csv файл
     with open('vine.csv', 'w', newline="", encoding="utf-8") as file:
         writer = csv.writer(file)
-        writer.writerow(['Баллы', 'Вино(описание)', 'Цена'])
+        writer.writerow(['Вино(описание)', 'Цена'])
         # Записываем в csv файл данные
         for info_vine in text:
             writer.writerow(info_vine)
 
 
+
 if __name__ == '__main__':
-    main()
+    url = input('Вставьте ссылку на страницу --> ')
+    # Проверка введенной ссылки
+    if '://' not in url or 'krasnoeibeloe' not in url:
+        print('\nВведите правильную ссылку!')
+    else:
+        main(url)
